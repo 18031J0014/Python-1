5b94cceb73e6be3a8b7bcc05fc38d52950780096
18031J0014 <44960336+18031J0014@users.noreply.github.com>
2019-07-08 19:01:00 +0530

    Update FolderCreation.py

--- stock/FolderCreation.py
+++ stock/FolderCreation.py
@@ -7,8 +7,8 @@ fh=open('store1.txt','r')
 for line in fh:
     str = line.split(',')
     st=str[1].rstrip()
-    path='C:/Users/Jyothshna/PycharmProjects/stock/%s' %st
-    s='C:/Users/Jyothshna/PycharmProjects/stock/'
+    path='C:/Users/Sowmya/PycharmProjects/stock/%s' %st
+    s='C:/Users/Sowmya/PycharmProjects/stock/'
     if(os.path.exists(path)):
         print("path already exists")
     else:
@@ -19,28 +19,28 @@ for line in fh:
 
     #summary
     summary=url+""+str[0]+"?p="+str[0]
-    new_link="C:/Users/Jyothshna/PycharmProjects/stock/" +str[1].rstrip()+"/summary.html"
+    new_link="C:/Users/Sowmya/PycharmProjects/stock/" +str[1].rstrip()+"/summary.html"
     r=requests.get(summary,stream=True)
     with open(new_link,'wb') as f:
         f.write(r.content)
         f.close()
 
     finance = url + "" + str[0]+"/"+htmls[1]+ "?p=" + str[0]
-    new_link = "C:/Users/Jyothshna/PycharmProjects/stock/" + str[1].rstrip() + "/financial.html"
+    new_link = "C:/Users/Sowmya/PycharmProjects/stock/" + str[1].rstrip() + "/financial.html"
     r = requests.get(finance, stream=True)
     with open(new_link, 'wb') as f:
         f.write(r.content)
         f.close()
 
     profile = url +""+ str[0] + "/" + htmls[0] + "?p=" + str[0]
-    new_link = "C:/Users/Jyothshna/PycharmProjects/stock/" + str[1].rstrip() + "/profile.html"
+    new_link = "C:/Users/Sowmya/PycharmProjects/stock/" + str[1].rstrip() + "/profile.html"
     r = requests.get(profile, stream=True)
     with open(new_link, 'wb') as f:
         f.write(r.content)
         f.close()
 
     statistics = url + "" + str[0] + "/" + htmls[2] + "?p=" + str[0]
-    new_link = "C:/Users/Jyothshna/PycharmProjects/stock/" + str[1].rstrip() + "/statistics.html"
+    new_link = "C:/Users/Sowmya/PycharmProjects/stock/" + str[1].rstrip() + "/statistics.html"
     r = requests.get(statistics, stream=True)
     with open(new_link, 'wb') as f:
         f.write(r.content)
